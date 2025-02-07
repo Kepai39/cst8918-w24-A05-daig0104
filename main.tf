d0ab2946f8842a3f57dd8fac1f1ec51c8388ac75
Catherine Daigle(Kepai) <35710612+Kepai39@users.noreply.github.com>
2025-02-07 00:38:18 -0500

    added Network interface connected to public ip

--- main.tf
+++ main.tf
@@ -94,7 +94,19 @@ resource "azurerm_subnet" "subnet1" {
   resource_group_name  = azurerm_resource_group.Lab5RG.name
   virtual_network_name = azurerm_virtual_network.labVN.name
   address_prefixes     = ["10.0.2.0/24"]
-  network_security_group_id = azurerm_network_security_group.lab5NSG.id
+}
+
+resource "azurerm_network_interface" "lab5NIC" {
+  name                = "lab5-nic"
+  location            = azurerm_resource_group.Lab5RG.location
+  resource_group_name = azurerm_resource_group.Lab5RG.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicip.id
+  }
 }
 
 
