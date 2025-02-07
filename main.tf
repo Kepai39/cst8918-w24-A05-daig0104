dfaea0952152902a80418be82c6b45f2b6ea1c5f
Catherine Daigle(Kepai) <35710612+Kepai39@users.noreply.github.com>
2025-02-07 00:07:12 -0500

    added subnet resource

--- main.tf
+++ main.tf
@@ -60,4 +60,10 @@ resource "azurerm_virtual_network" "labVN" {
   address_space       = ["10.0.0.0/16"]
 }
 
-
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.Lab5RG.name
+  virtual_network_name = azurerm_virtual_network.labVN.name
+  address_prefixes     = ["10.0.1.0/24"]  # Subnet within the VNet
+  #network_security_group_id = azurerm_network_security_group.example_nsg.id
+}
